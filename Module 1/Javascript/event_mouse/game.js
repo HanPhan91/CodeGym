424289a6c1612ee9576e2efdc1f477611690e80b
HanPhan91 <phangiahan1991@gmail.com>
2021-10-15 16:05:10 +0700

    Event Mouse

--- Module 1/Javascript/event_mouse/game.js
+++ Module 1/Javascript/event_mouse/game.js
@@ -4,20 +4,29 @@ function initgame() {
 let widthScreen = window.innerWidth;
 yPosition = 0;
 widthBall = 100;
-let move = 10;
+let move = 20;
 let select_img = document.getElementById('ball');
+let rotate=180;
 initgame();
 
 function turnLeft() {
     yPosition = select_img.offsetLeft;
     if (yPosition!=0) {
         select_img.style.left = `${yPosition- move}px`;
+        // select_img.style.transform= "rotateX(0deg)";
     }
 }
 
 function turnRight() {
+    select_img.classList.add('run');
     yPosition = select_img.offsetLeft;
     if ((yPosition + widthBall) < widthScreen) {
         select_img.style.left = `${yPosition+ move}px`;
+        // select_img.style.animationName= "rotate";
+        
+        setTimeout(() => {
+            select_img.classList.remove('run'); 
+        }, 500);
+       
     }
 }
