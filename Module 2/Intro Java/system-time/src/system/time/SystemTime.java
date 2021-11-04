70e1d094f28ddce9ff8553f9e88b6cff91d52663
HanPhan91 <phangiahan1991@gmail.com>
2021-11-04 13:55:56 +0700

    [Thực hành] Hiển thị thời gian của hệ thống

---
+++ Module 2/Intro Java/system-time/src/system/time/SystemTime.java
@@ -0,0 +1,10 @@
+package system.time;
+import java.util.Date;
+public class SystemTime {
+
+    public static void main(String[] args) {
+	// write your code here
+        Date now= new Date();
+        System.out.println("Now is: " + now);
+    }
+}
