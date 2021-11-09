a158cc9698009661d73ea4fddb305853cb87411b
HanPhan91 <phangiahan1991@gmail.com>
2021-11-09 10:07:11 +0700

    [Bài tập] Xây dựng lớp QuadraticEquation (Phương trình bậc hai)

---
+++ Module 2/OOP/quadratic-equation/src/com/codegym/QuadraticEquation.java
@@ -0,0 +1,36 @@
+package com.codegym;
+
+public class QuadraticEquation {
+    double a, b, c;
+
+    public QuadraticEquation() {
+    }
+
+    public void setNumber(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getDiscriminant() {
+        return ((b * b) - (4 * a * c));
+    }
+
+    public double getRoot1() {
+        return ((-b + Math.pow((b * b) - (4 * a * c), 0.5)) / 2 * a);
+    }
+
+    public double getRoot2() {
+        return ((-b - Math.pow((b * b - 4 * a * c), 0.5)) / 2 * a);
+    }
+
+    public double getDoubleRoot() {
+        return -b / a;
+    }
+}
