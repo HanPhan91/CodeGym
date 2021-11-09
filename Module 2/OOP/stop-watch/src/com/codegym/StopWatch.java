aa228aa37c1f6352829a5b6b97dec978fb5e1f35
HanPhan91 <phangiahan1991@gmail.com>
2021-11-09 16:29:13 +0700

    [Bài tập] Xây dựng lớp StopWatch

---
+++ Module 2/OOP/stop-watch/src/com/codegym/StopWatch.java
@@ -0,0 +1,39 @@
+package com.codegym;
+
+import java.time.*;
+
+public class StopWatch {
+    LocalTime startTime;
+    LocalTime endTime;
+
+    public StopWatch() {
+    }
+
+    private LocalTime getStartTime() {
+//        return java.time.LocalTime.now();
+        ZoneId z = ZoneId.of("Asia/Ho_Chi_Minh");
+        return startTime = LocalTime.now();
+    }
+
+    private LocalTime getEndTime() {
+//        return java.time.LocalTime.now();
+        ZoneId z = ZoneId.of("Asia/Ho_Chi_Minh");
+        return endTime = LocalTime.now();
+    }
+
+    public void start() {
+        getStartTime();
+        System.out.println(getStartTime());
+    }
+
+    public void stop() {
+        getEndTime();
+        System.out.println(getEndTime());
+    }
+
+    public long getElapsedTime() {
+        int a=startTime.getNano();
+        int b=endTime.getNano();
+        return (b-a)/1000000;
+    }
+}
