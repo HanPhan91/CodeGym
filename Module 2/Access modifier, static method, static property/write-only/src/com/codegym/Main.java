9010147f33959c0be0a05232f750ed261fd46a49
HanPhan91 <phangiahan1991@gmail.com>
2021-11-10 14:03:14 +0700

    [Bài tập] Xây dựng lớp chỉ ghi trong Java

---
+++ Module 2/Access modifier, static method, static property/write-only/src/com/codegym/Main.java
@@ -0,0 +1,11 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Student Test= new Student();
+        Test.setName("abc");
+        Test.setClasses("c03");
+    }
+}
