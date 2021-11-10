410b94f0169b8a0a7a99b96ec8012f719cff7bab
HanPhan91 <phangiahan1991@gmail.com>
2021-11-10 13:55:35 +0700

    [Bài tập] Access modifier

---
+++ Module 2/Access modifier, static method, static property/access-modifier/src/com/codegym/Main.java
@@ -0,0 +1,11 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Circle TestCircle = new Circle(5);
+        System.out.println(TestCircle.getRadius());
+        System.out.println(TestCircle.getArea());
+    }
+}
