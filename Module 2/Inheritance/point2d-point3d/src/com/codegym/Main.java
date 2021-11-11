34bfcdcaf0d0809fc79d604baee7f3c37d957fdd
HanPhan91 <phangiahan1991@gmail.com>
2021-11-11 11:20:26 +0700

    [Bài tập] Lớp Point2D và lớp Point3D

---
+++ Module 2/Inheritance/point2d-point3d/src/com/codegym/Main.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Point2D point2d= new Point2D(3.2f, 2.7f);
+        Point2D point3d= new Point3D(1.7f,4f,9.8f);
+        System.out.println(point2d.toString());
+
+        System.out.println(point3d.toString());
+    }
+}
