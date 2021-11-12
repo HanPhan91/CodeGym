d81c3da4811dc277cc57a2f33f90f091a4146707
HanPhan91 <phangiahan1991@gmail.com>
2021-11-12 15:17:22 +0700

    [Bài tập] triển khai interface 'Colorable' cho các lớp hình học

---
+++ Module 2/Abstract Class & Interface/colorable-shape/src/com/codegym/Main.java
@@ -0,0 +1,11 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Shape shape= new Rectangle(40,12);
+        shape.setColor("red");
+        shape.howToColor();
+    }
+}
