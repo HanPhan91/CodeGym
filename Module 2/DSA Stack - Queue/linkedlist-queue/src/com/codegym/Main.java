bfb7208aa72f2145d1b7db44cdc5c3ecacf87afe
HanPhan91 <phangiahan1991@gmail.com>
2021-11-19 14:33:55 +0700

    [Thực hành] Triển khai Queue sử dụng danh sách liên kết

---
+++ Module 2/DSA Stack - Queue/linkedlist-queue/src/com/codegym/Main.java
@@ -0,0 +1,16 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+class Node{
+    public int key;
+    public Node next;
+    public Node(int key){
+        this.key=key;
+        this.next=null;
+    }
+}
